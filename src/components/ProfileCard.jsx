c643d5bdf60dd3573107f9e9bf29e14d9403b361
OleksandrShchur0008 <sasha28shchur@gmail.com>
2024-02-15 19:17:17 +0100

    homework  lesson17

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,20 @@
+import "./ProfileCard.css";
+
+function ProfileCard() {
+  return (
+    <div className="profile-card">
+      <img
+        className="img-giannis"
+        src="https://cdn.nba.com/teams/uploads/sites/1610612749/2022/10/2223_POTG_Cream_Artboard-4.jpg"
+        alt="Giannis img"
+      />
+      <h1>Name: Giannis Antetokounmpo</h1>
+      <h3>Profession: Professional Basketball Player</h3>
+      <h3>Hair color: Black</h3>
+      <h3>Height: 2,11m</h3>
+      <h3>Hobby: guitar playing</h3>
+    </div>
+  );
+}
+
+export default ProfileCard;
